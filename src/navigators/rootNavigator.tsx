ab4d42e2a7e0818ea490f5d6db7743e8e3f351ed
Harvey Chui <chuiharvey@gmail.com>
2020-09-07 21:58:15 -0700

    Added Typing to the Navigator

--- src/navigators/rootNavigator.tsx
+++ src/navigators/rootNavigator.tsx
@@ -4,12 +4,20 @@ import {createStackNavigator} from '@react-navigation/stack';
 /* Screens Stack */
 import HomeScreen from '../containers/Home';
 import LoginScreen from '../containers/Login';
+import RegisterScreen from '../containers/Register';
 
-const RootStack = createStackNavigator();
+export type RootStackParamList = {
+  Home: undefined;
+  Login: undefined;
+  Register: undefined;
+};
+
+const RootStack = createStackNavigator<RootStackParamList>();
 
 const RootStackNavigator = () => (
   <RootStack.Navigator>
     <RootStack.Screen name="Login" component={LoginScreen} />
+    <RootStack.Screen name="Register" component={RegisterScreen} />
     <RootStack.Screen name="Home" component={HomeScreen} />
   </RootStack.Navigator>
 );
